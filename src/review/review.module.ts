fe7dab5278b587e30199aefa27f11e98c1308453
Nikita Fokin <nikita.fokin0123@gmail.com>
2025-04-18 23:39:30 +0500

    fix bugs erors and add provider ReviewService

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { ReviewService } from './review.service';
+import { ReviewModel } from './review.model';
 
 @Module({
-  controllers: [ReviewController],
-  imports: [
-      TypegooseModule.forFeature([{
-        typegooseClass: ReviewModule,
-        schemaOptions: {
-          collection: 'Review'
-        }
-      }])
-    ]
+	controllers: [ReviewController],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: ReviewModel,
+				schemaOptions: {
+					collection: 'Review'
+				}
+			}
+		])
+	],
+	providers: [ReviewService]
 })
-export class ReviewModule {}
+export class ReviewModule { }
+
